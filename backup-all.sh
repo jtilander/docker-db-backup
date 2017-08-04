e836a9ef7fc775460897df7c337bae334b3f6dd8
Jim Tilander <jim@tilander.org>
2017-08-03 22:52:16 -0700

    Normalized the output from the container

--- backup-all.sh
+++ backup-all.sh
@@ -12,11 +12,11 @@ exec 1>&2
 set -o pipefail
 
 if curl -SsL http://rancher-metadata.rancher.internal/latest/self/host/hostname > /dev/null 2>&1; then
-	echo "Detecting rancher metadata, getting the hostname from Rancher..."
+	echo "INFO     Detecting rancher metadata, getting the hostname from Rancher..."
 	export HOSTNAME=`curl -SsL http://rancher-metadata.rancher.internal/latest/self/host/hostname`
 fi
 
-echo "Backing up all database containers on host $HOSTNAME"
+echo "INFO     Backing up all database containers on host $HOSTNAME"
 
 ( /app/backup-all.py "$1" )
 
